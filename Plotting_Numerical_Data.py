d777c9d780a745bc4f0455647b6a8c1bf691c2d7
Juan Sebastian Roa <65826708+jsroa15@users.noreply.github.com>
2020-12-14 15:57:05 -0500

    Create Plotting_Numerical_Data.py

---
+++ Plotting_Numerical_Data.py
@@ -0,0 +1,23 @@
+def plotting_numerical(dataf,rows,cols,x_list,size):
+  '''Plot numerical data in subplots using Seaborn
+
+  Parameters
+  --------------
+    data:Data frame with data
+    rows:Number of rows in subplot
+    cols:Number of columns in subplot
+    x_list:List with features names
+    size:Tupple with size of plot
+    
+  '''
+  sns.set_style("whitegrid")
+
+  pos=0
+  f, axes = plt.subplots(nrows=rows, ncols=cols,figsize=size)
+  for i in range(0,rows):
+    for j in range(0,cols):
+      
+      sns.kdeplot(data=dataf,ax=axes[i,j],x=x_list[pos])
+      pos+=1
+
+  return(plt.show())
