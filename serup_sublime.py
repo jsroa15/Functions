1254027738a17996e86eec1a3602fe10226575e5
Juan Sebastian Roa <65826708+jsroa15@users.noreply.github.com>
2021-02-22 20:26:16 -0500

    Create serup_sublime.py

---
+++ serup_sublime.py
@@ -0,0 +1,5 @@
+{
+	"cmd": ["C:\\Users\\Juan Sebastian\\.conda\\envs\\new_base\\python.exe", "-i", "-u", "$file"],
+    "file_regex": "^[ ]file \"(...?)\", line ([0-9]*)",
+    "selector": "source.python"
+}
